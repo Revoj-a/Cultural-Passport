2e414ac42afee55ff980badaad8c251727daea9e
Revoj-a <revojjoever@gmail.com>
2025-12-30 22:59:15 +0800

    Build search input

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,11 @@
 import { HStack, Image } from "@chakra-ui/react";
 import dice from "../assets/dice.png";
+import SearchInput from "./SearchInput";
 
 const NavBar = () => {
   return (
     <HStack padding="10px">
+      <SearchInput />
       <Image src={dice} boxSize="35px" objectFit="cover" />
     </HStack>
   );
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,5 +1,21 @@
+import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import { useRef } from "react";
+import { BsSearch } from "react-icons/bs";
+
 const SearchInput = () => {
-  return <div>SearchInput</div>;
+  const ref = useRef<HTMLInputElement>(null);
+  return (
+    <InputGroup>
+      <InputLeftElement children={<BsSearch />}></InputLeftElement>
+      <Input
+        ref={ref}
+        borderRadius={20}
+        placeholder="Random Country"
+        variant="filled"
+        _placeholder={{ color: "white", fontWeight: "bold" }}
+      />
+    </InputGroup>
+  );
 };
 
 export default SearchInput;
